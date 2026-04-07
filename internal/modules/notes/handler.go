d5d49eeccc39d578cad4785061044a2716058a45
Ej <elnesjani@gmail.com>
2026-04-07 14:57:57 +0800

    feat:added handler for CreateNote

--- internal/modules/notes/handler.go
+++ internal/modules/notes/handler.go
@@ -1,7 +1,41 @@
 package notes
 
+import (
+	"log"
+	"net/http"
+
+	repo "github.com/Ej0416/go-note-app/internal/adapters/postgresql/sqlc"
+	"github.com/Ej0416/go-note-app/internal/json"
+	"github.com/Ej0416/go-note-app/internal/types"
+)
 
 func NewHandler(service Service) *handler {
 	return &handler{service: service}
 }
 
+func (h *handler) CreateNote(w http.ResponseWriter, r *http.Request) {
+	var req repo.CreateNoteParams
+	if err := json.Read(r, &req); err != nil {
+		log.Printf("error parsing request params: %s ", err)
+		json.Write(w, http.StatusBadRequest, types.APIResponse{
+			Success: false,
+			Error: "invalild request body",
+		})
+		return
+	}
+
+	err := h.service.CreateNote(r.Context(), req)
+	if err != nil {
+		log.Printf("failed to create note: %s ", err)
+		json.Write(w, http.StatusBadRequest, types.APIResponse{
+			Success: false,
+			Error: "failed to create note",
+		})
+		return
+	}
+
+	json.Write(w, http.StatusOK, types.APIResponse{
+		Success: true,
+		Data: nil,
+	})
+}
